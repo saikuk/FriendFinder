5dbf4da25f66cc54480f911402b7a4827dc06e95
Sai Kuk <kukcheong@gmail.com>
2019-02-20 17:46:28 -0500

    Upload HW 13

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "name": "Sai",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwicgaftscvgAhUyUt8KHSxFD74QjRx6BAgBEAU&url=https%3A%2F%2Fwww.independent.co.uk%2Fnews%2Fworld%2Famericas%2Fmonkey-selfie-david-slater-photographer-peta-copyright-image-camera-wildlife-personalities-macaques-a7941806.html&psig=AOvVaw2jjSLqHB2p76TkaY71mDpP&ust=1550789132517934",
     "scores": [
     "5",
     "1",
@@ -16,8 +16,8 @@ var friendsArray = [
     ]
     },
     {
-    "name": "Jacob Deming",
-    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "name": "Cheong",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwicgaftscvgAhUyUt8KHSxFD74QjRx6BAgBEAU&url=https%3A%2F%2Fwww.independent.co.uk%2Fnews%2Fworld%2Famericas%2Fmonkey-selfie-david-slater-photographer-peta-copyright-image-camera-wildlife-personalities-macaques-a7941806.html&psig=AOvVaw2jjSLqHB2p76TkaY71mDpP&ust=1550789132517934",
     "scores": [
     "4",
     "2",
@@ -32,8 +32,8 @@ var friendsArray = [
     ]
     },
     {
-    "name": "Jeremiah Scanlon",
-    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "name": "John Smith",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwicgaftscvgAhUyUt8KHSxFD74QjRx6BAgBEAU&url=https%3A%2F%2Fwww.independent.co.uk%2Fnews%2Fworld%2Famericas%2Fmonkey-selfie-david-slater-photographer-peta-copyright-image-camera-wildlife-personalities-macaques-a7941806.html&psig=AOvVaw2jjSLqHB2p76TkaY71mDpP&ust=1550789132517934",
     "scores": [
     "5",
     "2",
@@ -48,8 +48,8 @@ var friendsArray = [
     ]
     },
     {
-    "name": "Louis T. Delia",
-    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "name": "Joe Yeung",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwicgaftscvgAhUyUt8KHSxFD74QjRx6BAgBEAU&url=https%3A%2F%2Fwww.independent.co.uk%2Fnews%2Fworld%2Famericas%2Fmonkey-selfie-david-slater-photographer-peta-copyright-image-camera-wildlife-personalities-macaques-a7941806.html&psig=AOvVaw2jjSLqHB2p76TkaY71mDpP&ust=1550789132517934",
     "scores": [
     "3",
     "3",
@@ -65,7 +65,7 @@ var friendsArray = [
     },
     {
     "name": "Lou Ritter",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwicgaftscvgAhUyUt8KHSxFD74QjRx6BAgBEAU&url=https%3A%2F%2Fwww.independent.co.uk%2Fnews%2Fworld%2Famericas%2Fmonkey-selfie-david-slater-photographer-peta-copyright-image-camera-wildlife-personalities-macaques-a7941806.html&psig=AOvVaw2jjSLqHB2p76TkaY71mDpP&ust=1550789132517934",
     "scores": [
     "4",
     "3",
@@ -75,326 +75,6 @@ var friendsArray = [
     "2",
     "5",
     "3",
-    "1",
-    "4"
-    ]
-    },
-    {
-    "name": "Jordan Biason",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
-    "scores": [
-    "4",
-    "4",
-    "2",
-    "3",
-    "2",
-    "2",
-    "3",
-    "2",
-    "4",
-    "5"
-    ]
-    },
-    {
-    "name": "Joan",
-    "photo": "https://en.wikipedia.org/wiki/Pocahontas#/media/File:Pocahontas_by_Simon_van_de_Passe_1616.jpg",
-    "scores": [
-    "5",
-    "3",
-    "3",
-    "2",
-    "4",
-    "2",
-    "4",
-    "3",
-    "2",
-    "2"
-    ]
-    },
-    {
-    "name": "pocha vargas",
-    "photo": "dfdfdfdfd",
-    "scores": [
-    "4",
-    "3",
-    "2",
-    "4",
-    "4",
-    "4",
-    "4",
-    "2",
-    "4",
-    "3"
-    ]
-    },
-    {
-    "name": "test",
-    "photo": "http://google.com/images/blah.jpg",
-    "scores": [
-    "4",
-    "5",
-    "5",
-    "3",
-    "4",
-    "2",
-    "3",
-    "4",
-    "3",
-    "3"
-    ]
-    },
-    {
-    "name": "test",
-    "photo": "https://stepupandlive.files.wordpress.com/2014/09/3d-animated-frog-image.jpg",
-    "scores": [
-    "4",
-    "5",
-    "5",
-    "3",
-    "4",
-    "2",
-    "3",
-    "4",
-    "3",
-    "3"
-    ]
-    },
-    {
-    "name": "test",
-    "photo": "https://stepupandlive.files.wordpress.com/2014/09/3d-animated-frog-image.jpg",
-    "scores": [
-    "4",
-    "5",
-    "5",
-    "3",
-    "4",
-    "2",
-    "3",
-    "4",
-    "3",
-    "3"
-    ]
-    },
-    {
-    "name": "test",
-    "photo": "https://stepupandlive.files.wordpress.com/2014/09/3d-animated-frog-image.jpg",
-    "scores": [
-    "4",
-    "5",
-    "5",
-    "3",
-    "4",
-    "2",
-    "3",
-    "4",
-    "3",
-    "3"
-    ]
-    },
-    {
-    "name": "Andrew Dupas",
-    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjAi7vJ1crgAhUqJTQIHfaPCsoQjRx6BAgBEAU&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AVanessa_Blue.jpg&psig=AOvVaw3d6Xm9Z3oLDxXp8Yx5qUZR&ust=1550763746677656",
-    "scores": [
-    "3",
-    "2",
-    "3",
-    "4",
-    "5",
-    "2",
-    "2",
-    "5",
-    "4",
-    "2"
-    ]
-    },
-    {
-    "name": "Andrew Dupas",
-    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjAi7vJ1crgAhUqJTQIHfaPCsoQjRx6BAgBEAU&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AVanessa_Blue.jpg&psig=AOvVaw3d6Xm9Z3oLDxXp8Yx5qUZR&ust=1550763746677656",
-    "scores": [
-    "3",
-    "2",
-    "3",
-    "4",
-    "5",
-    "2",
-    "2",
-    "5",
-    "4",
-    "2"
-    ]
-    },
-    {
-    "name": "Andrew Dupas",
-    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjAi7vJ1crgAhUqJTQIHfaPCsoQjRx6BAgBEAU&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AVanessa_Blue.jpg&psig=AOvVaw3d6Xm9Z3oLDxXp8Yx5qUZR&ust=1550763746677656",
-    "scores": [
-    "3",
-    "2",
-    "3",
-    "4",
-    "5",
-    "2",
-    "2",
-    "5",
-    "4",
-    "2"
-    ]
-    },
-    {
-    "name": "Andrew Dupas",
-    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjAi7vJ1crgAhUqJTQIHfaPCsoQjRx6BAgBEAU&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AVanessa_Blue.jpg&psig=AOvVaw3d6Xm9Z3oLDxXp8Yx5qUZR&ust=1550763746677656",
-    "scores": [
-    "3",
-    "2",
-    "3",
-    "4",
-    "5",
-    "2",
-    "2",
-    "5",
-    "4",
-    "2"
-    ]
-    },
-    {
-    "name": "Kevin",
-    "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1531804055935-76f44d7c3621%3Fixlib%3Drb-1.2.1%26ixid%3DeyJhcHBfaWQiOjEyMDd9%26auto%3Dformat%26fit%3Dcrop%26w%3D1000%26q%3D80&imgrefurl=https%3A%2F%2Funsplash.com%2Fphotos%2F75_s8iWHKLs&docid=aNIlimxbhnamdM&tbnid=PRHKizBsSqGMVM%3A&vet=10ahUKEwi-ncvi2MrgAhUs8KYKHRQrDW0QMwhrKAAwAA..i&w=1000&h=1498&bih=689&biw=1280&q=photo&ved=0ahUKEwi-ncvi2MrgAhUs8KYKHRQrDW0QMwhrKAAwAA&iact=mrc&uact=8",
-    "scores": [
-    "3",
-    "2",
-    "4",
-    "5",
-    "3",
-    "2",
-    "4",
-    "1",
-    "4",
-    "3"
-    ]
-    },
-    {
-    "name": "Carter",
-    "photo": "https://theoffice.fandom.com/wiki/Andy_Bernard?file=Andy_Bernard.jpg",
-    "scores": [
-    "3",
-    "5",
-    "2",
-    "3",
-    "1",
-    "5",
-    "2",
-    "5",
-    "2",
-    "3"
-    ]
-    },
-    {
-    "name": "Sample",
-    "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.maxpixel.net%2Fstatic%2Fphoto%2F1x%2FModel-Female-Headshot-Pose-Portrait-3440014.jpg&imgrefurl=https%3A%2F%2Fwww.maxpixel.net%2FModel-Female-Headshot-Pose-Portrait-3440014&docid=zV1Gims1zPktGM&tbnid=xMELnMzdw7nBOM%3A&vet=10ahUKEwjDibzq_8rgAhXDV98KHbrdCHQQMwiaASgiMCI..i&w=869&h=720&bih=690&biw=1280&q=headshot%20photo&ved=0ahUKEwjDibzq_8rgAhXDV98KHbrdCHQQMwiaASgiMCI&iact=mrc&uact=8",
-    "scores": [
-    "1",
-    "1",
-    "2",
-    "1",
-    "3",
-    "2",
-    "2",
-    "2",
-    "1",
-    "1"
-    ]
-    },
-    {
-    "name": "aaaaa",
-    "photo": "aaaaa",
-    "scores": [
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "1"
-    ]
-    },
-    {
-    "name": "test",
-    "photo": "https://i.imgur.com/1tpvNmh.jpg",
-    "scores": [
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5"
-    ]
-    },
-    {
-    "name": "Steve Jones",
-    "photo": "https://media.licdn.com/dms/image/C4E03AQGhSq1ZhhyLXA/profile-displayphoto-shrink_200_200/0?e=1556150400&v=beta&t=MBAahY43X4Wq3zvOIua8bzyiBq2G6qE82_ZFP1jOwQU",
-    "scores": [
-    "2",
-    "4",
-    "2",
-    "4",
-    "5",
-    "3",
-    "3",
-    "1",
-    "4",
-    "3"
-    ]
-    },
-    {
-    "name": "Summer",
-    "photo": "https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest?cb=20160919183158",
-    "scores": [
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5",
-    "5"
-    ]
-    },
-    {
-    "name": "Apple",
-    "photo": "https://www.apple.com/ac/structured-data/images/knowledge_graph_logo.png?201606271147",
-    "scores": [
-    "1",
-    "1",
-    "1",
-    "3",
-    "1",
-    "1",
-    "1",
-    "3",
-    "1",
-    "1"
-    ]
-    },
-    {
-    "name": "mick",
-    "photo": "https://gph.is/1Tm6BS2",
-    "scores": [
-    "5",
-    "2",
-    "3",
-    "2",
-    "3",
-    "4",
-    "4",
-    "4",
     "1",
     "4"
     ]
